0bd340ad6c70501655a030e5a98f56a8c0b2c623
ybz <1615498405@qq.com>
2021-05-03 10:48:42 +0800

    spring

--- src/main/java/com/yeah/domain/Student.java
+++ src/main/java/com/yeah/domain/Student.java
@@ -1,9 +1,13 @@
 package com.yeah.domain;
 
+import lombok.Data;
+
 /**
  * @author: yeah
  * lombok 的作用：自动生成set和get方法
  */
-
+@Data
 public class Student {
+    private String name;
+    private int age;
 }
---
+++ src/main/java/com/yeah/spring/Definition.java
@@ -0,0 +1,21 @@
+package com.yeah.spring;
+
+/**
+ * @author: yeah
+ */
+public class Definition {
+    /**
+     * 什么是Spring?
+     *  Spring是一个开放源代码的设计层面框架
+     *  他解决的是业竻逻辑层和其他各层的松耦合问题,因此它将面向接口的编程思想贯穿整个系统应用
+     *  Spring是于2003年兴起的个轻量级的Java开发框架
+     *  简单来说, Spring是个分层的 JavaSE/EE full-stack(站式轻量级开源框架)
+     *
+     *spring 特点
+     *  1方便解耦,简化开发
+     *  2AOP编程的支持
+     *  3声明式事务的支持
+     *  4方便程序的测试
+     *  5方便集成各种优秀框架
+     */
+}
